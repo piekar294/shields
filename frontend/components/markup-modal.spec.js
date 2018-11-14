e04cdaae188c8689aeaac4b305020d9c4f826d46
Michal Piekarczyk <michal.piekarczyk@allegro.pl>
2018-11-14 21:49:56 +0100

    better test description

--- frontend/components/markup-modal.spec.js
+++ frontend/components/markup-modal.spec.js
@@ -9,48 +9,50 @@ const { expect } = chai
 Enzyme.configure({ adapter: new Adapter() })
 
 describe('markup modal', function() {
-  it('should not override default state on constructing modal with empty example prop', function() {
-    // given
-    const props = {
-      example: null,
-      baseUrl: 'http://localhost:8080',
-      onRequestClose: () => {},
-    }
+  describe('constructor', function() {
+    it('should not override default state when example prop is empty', function() {
+      // given
+      const props = {
+        example: null,
+        baseUrl: 'http://localhost:8080',
+        onRequestClose: () => {},
+      }
 
-    // when
-    const markupModalWrapper = shallow(<MarkupModal {...props} />)
+      // when
+      const markupModalWrapper = shallow(<MarkupModal {...props} />)
 
-    // then
-    expect(markupModalWrapper.state()).to.deep.equal({
-      exampleUrl: null,
-      badgeUrl: null,
-      link: '',
-      style: 'flat',
+      // then
+      expect(markupModalWrapper.state()).to.deep.equal({
+        exampleUrl: null,
+        badgeUrl: null,
+        link: '',
+        style: 'flat',
+      })
     })
-  })
 
-  it('should override state on constructing modal with example prop given', function() {
-    // given
-    const props = {
-      example: {
-        title: 'Coveralls github',
-        exampleUrl: '/coveralls/github/jekyll/jekyll.svg',
-        previewUrl: '/coveralls/github/jekyll/jekyll.svg',
+    it('should override default state based on example prop', function() {
+      // given
+      const props = {
+        example: {
+          title: 'Coveralls github',
+          exampleUrl: '/coveralls/github/jekyll/jekyll.svg',
+          previewUrl: '/coveralls/github/jekyll/jekyll.svg',
+          link: 'test-link',
+        },
+        baseUrl: 'http://localhost:8080',
+        onRequestClose: () => {},
+      }
+
+      // when
+      const markupModalWrapper = shallow(<MarkupModal {...props} />)
+
+      // then
+      expect(markupModalWrapper.state()).to.deep.equal({
+        exampleUrl: 'http://localhost:8080/coveralls/github/jekyll/jekyll.svg',
+        badgeUrl: 'http://localhost:8080/coveralls/github/jekyll/jekyll.svg',
         link: 'test-link',
-      },
-      baseUrl: 'http://localhost:8080',
-      onRequestClose: () => {},
-    }
-
-    // when
-    const markupModalWrapper = shallow(<MarkupModal {...props} />)
-
-    // then
-    expect(markupModalWrapper.state()).to.deep.equal({
-      exampleUrl: 'http://localhost:8080/coveralls/github/jekyll/jekyll.svg',
-      badgeUrl: 'http://localhost:8080/coveralls/github/jekyll/jekyll.svg',
-      link: 'test-link',
-      style: 'flat',
+        style: 'flat',
+      })
     })
   })
 })
