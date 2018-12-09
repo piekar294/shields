25777d50d2eb60f570a866072bfb53b9506fa601
Michal Piekarczyk <michal.piekarczyk@allegro.pl>
2018-12-09 20:44:59 +0100

    simple markup modal test

--- frontend/components/markup-modal.spec.js
+++ frontend/components/markup-modal.spec.js
@@ -2,6 +2,7 @@ import Enzyme, { shallow } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import MarkupModal from './markup-modal'
 import React from 'react'
+import Modal from 'react-modal'
 
 const chai = require('chai')
 const { expect } = chai
@@ -9,7 +10,7 @@ const { expect } = chai
 Enzyme.configure({ adapter: new Adapter() })
 
 describe('markup modal', function() {
-  it('should render markup modal with missing example', function() {
+  it('should render closed modal', function() {
     // given
     const props = {
       baseUrl: 'http://localhost:8080',
@@ -20,12 +21,6 @@ describe('markup modal', function() {
     const markupModalWrapper = shallow(<MarkupModal {...props} />)
 
     // then
-    expect(markupModalWrapper.state()).to.deep.equal({
-      badgeUrl: '',
-      badgeUrlForProps: '',
-      exampleUrl: '',
-      link: '',
-      style: 'flat',
-    })
+    expect(markupModalWrapper.find(Modal).prop('isOpen')).to.be.false
   })
 })
